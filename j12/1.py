bd3bad9747d81e29b55b153f83a15531a88482ee
Shayan Shahbazi <upm200@gmail.com>
2024-08-11 20:16:48 +0330

    j12

---
+++ j12/1.py
@@ -0,0 +1,7 @@
+a = "hello"
+b = 23
+lst = []
+print(type(a))
+print(type(b))
+print(type(lst.append))
+a = input().split()
---
+++ j12/3.py
@@ -0,0 +1,11 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def sleep():
+        print("I'm sleeping")
+
+
+s1 = Person("ali", 20)
+print(s1.sleep)
